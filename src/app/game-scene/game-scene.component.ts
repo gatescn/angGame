cda86884f32c361b2f3b39272089d7ab9af822ed
gatescn <gatescn@vcu.edu>
2019-04-16 14:07:28 -0400

    3 files modified, added player card logic

--- src/app/game-scene/game-scene.component.ts
+++ src/app/game-scene/game-scene.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {PlayerCard} from './player-card';
+import {PlayerInformation} from './player-information'
 
 @Component({
   selector: 'game-scene',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameSceneComponent implements OnInit {
 
+
+  NumberOfPlayers: number = 2;
+  CardDeck: PlayerCard[] = null;
+
+
   constructor() { }
 
   ngOnInit() {
+    for (var _i = 0; _i < this.NumberOfPlayers; _i++) {
+        this.AddPlayers();
+    }
+  }
+  AddPlayers(){
+   
+    this.CardDeck.push(
+
+    )
+
+
   }
 
 }
--- src/app/game-scene/player-card.ts
+++ src/app/game-scene/player-card.ts
@@ -1,7 +1,17 @@
 import{ CardTypes } from './card-types.enum';
+
 export class PlayerCard {
   CardOwner: String = "";
   CardType: CardTypes = CardTypes.None;
   RowSize: number = 1;
   ColumnSize: number = 2;
+
+  constructor(owner:string, cardType: CardTypes,
+   rowSize: number, colSize: number)
+  {
+    this.CardOwner = owner;
+    this.CardType = cardType;
+    this.RowSize = rowSize;
+    this.ColumnSize = colSize;
+  }
 }

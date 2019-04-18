88fcbaa4b35536cbc0d2cb039e1e4b47ecda6c75
gatescn <gatescn@vcu.edu>
2019-04-18 14:41:32 -0400

    1 binaries uploaded

--- src/app/game-scene/game-scene.component.ts
+++ src/app/game-scene/game-scene.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {PlayerCard} from './player-card';
-import {PlayerInformation} from './player-information'
+import {PlayerInformation} from './player-information';
 import {DefaultplayerData} from './defaultplayer-data';
+import { ImageContainer } from './image-container';
 
 @Component({
   selector: 'game-scene',
@@ -14,7 +15,7 @@ export class GameSceneComponent implements OnInit {
   NumberOfPlayers: number = 2;
   data: DefaultplayerData = new DefaultplayerData();
   CardDeck: PlayerCard[] = [];
-
+  images: ImageContainer = new ImageContainer();
 
   constructor() { }
 
@@ -24,6 +25,7 @@ export class GameSceneComponent implements OnInit {
     for (var _i = 0; _i < this.NumberOfPlayers; _i++) {
         this.AddPlayers(dummyData[_i]);
         console.log(this.CardDeck[_i]);
+        
 
     }
   }
---
+++ src/app/game-scene/image-container.ts
@@ -0,0 +1,10 @@
+export class ImageContainer {
+  rockPicture: string = "./rock.png";
+  paperPicture: string = "./paper.png";
+  scissorsPicture: string = "./scissors.png";
+  catPicture: string = "./cat.png";
+  
+  public constructor(){}
+
+
+}
